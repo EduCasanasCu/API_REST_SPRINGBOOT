14ab006d67abf497664f008bee53494b661aaef1
edu casanas <jorge.casanascue@ug.edu.ec>
2025-03-15 01:25:12 -0500

    Carga datos de prueba en mySQL

---
+++ src/main/java/com/application/rest/entities/Product.java
@@ -0,0 +1,30 @@
+package com.application.rest.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "producto")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre_producto")
+    private String name;
+
+    @Column(name = "precio_producto")
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "id_fabricante", nullable = false)
+    private Maker maker;
+}
---
+++ src/main/java/com/application/rest/persistence/IMakerDAO.java
@@ -0,0 +1,4 @@
+package com.application.rest.persistence;
+
+public interface IMakerDAO {
+}
